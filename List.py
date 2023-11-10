b16f48a5b77ad8124d95e0fe940493e13595aa34
fajarardiansyahh <ardiansyahfajar9499@gmail.com>
2023-11-11 00:34:17 +0700

    first commit

---
+++ List.py
@@ -0,0 +1,10 @@
+list = ["dana", "andi,", "novi", "rara", "udin", "sasa"]
+
+print ('Nama siswa:', list)
+print ('Jumlah siswa:', len(list))
+
+print('---------------------------------------------------------')
+
+del  list[0]
+print(list)
+print('Sisa siswa:', len(list))
---
+++ python.py
@@ -0,0 +1,12 @@
+print("======projek 4======")
+print('remidial uts')
+
+nilai = 90
+
+if nilai < 90:
+    print("anda tidak remidial")
+else :
+    print("anda remidial")
+
+
+
